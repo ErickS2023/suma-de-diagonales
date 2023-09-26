05dfda02abcb4e468d70a1cc582dcf1320590a0b
ErickS2023 <141955911+ErickS2023@users.noreply.github.com>
2023-09-25 20:55:55 -0600

    Add files via upload

---
+++ suma de diagonales.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+//suma de diagonales
+
+int main(){
+	int a[5] [5]={{0,0,7,1,0},{1,2,4,5,1},{2,4,2,3,2},{3,6,0,4,3},{4,8,2,5,4}};
+	int i,j;
+	
+	for(i=0; i<5; i++){
+		for(j=0; j<1; j++){
+			printf("%d",a[i][j]);
+			printf("    %d", a[i][j+1]);
+			printf("    %d", a[i][j+2]);
+			printf("    %d", a[i][j+3]);
+			printf("    %d\n",a[i][j+4]);
+		}
+	}
+	
+	
+	
+	
+	return 0;
+}
